6ddba9f22c86095fb39784d1470c49952cc7bc59
Амира Колчакова <kolchakovaar@std.tyuiu.ru>
2023-09-29 15:22:19 +0300

    Task6

---
+++ Tyuiu.KolchakovaA.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KolchakovaA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KolchakovaA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d2626d3-b312-465b-a9e8-fbf425ad48ac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KolchakovaA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KolchakovaA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.KolchakovaA.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] numArray = new int[] { 1, 2, 3, 4, 5, };
+
+            //Пример циклической структуры находитя в библиотеке классов в методе AdditionArray
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numArray));
+
+            //Пример циклической структуры находитя в библиотеке классов в методе SubtactioArray
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numArray));
+
+            //Пример циклической структуры находитя в библиотеке классов в методе MultplicationArray
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numArray));
+             Console.ReadKey();
+        }
+
+    }
+}
